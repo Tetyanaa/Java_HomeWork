55c58f420810d256ab4cf5b2bd1a4f0c410350f8
Tetyana Plysyuk <Tetyana_Plysyuk@epam.com>
2022-08-01 12:25:50 +0300

    Java Basic Module4 5.2

---
+++ src/main/java/com/tplysyuk/module5_1/Square.java
@@ -0,0 +1,10 @@
+package com.tplysyuk.module5_1;
+
+    class Square extends Rectangular{
+
+    public Square (int length1, int wide1) {
+        length = length1;
+        wide = wide1;
+
+    }
+}
